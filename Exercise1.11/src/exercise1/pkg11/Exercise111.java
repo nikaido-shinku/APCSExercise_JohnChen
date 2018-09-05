9a95bf7764adf12c5590141f252d64354ea376a8
nikaido-shinku <cjykuwabara@gmail.com>
2018-09-05 14:18:30 -0500

    1.11

---
+++ Exercise1.11/src/exercise1/pkg11/Exercise111.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise1.pkg11;
+
+/**
+ *
+ * @author 1
+ */
+public class Exercise111 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String myName = "John";
+       
+       myName = myName + " Chen";
+       myName += "J.C.";
+        System.out.println("Name = " + myName);
+    }
+    
+}
